bba4a191e1bfae21dfe3cd0527ac02a720060978
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-13 14:21:36 +0200

    remove verbose

--- monitor.sh
+++ monitor.sh
@@ -5,5 +5,5 @@ pvc_path=$(df --output=target | grep home)
 rsync -avz ${pvc_path}/jovyan /home
 
 while inotifywait -r -e modify,create,delete,move /home/jovyan; do
-    rsync -avz --delete /home/jovyan $pvc_path
+    rsync -az --delete /home/jovyan $pvc_path
 done
