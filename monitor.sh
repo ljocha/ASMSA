2aaab29e5134ee6820db45c482c9954d35539792
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-13 14:19:09 +0200

    added delete

--- monitor.sh
+++ monitor.sh
@@ -5,5 +5,5 @@ pvc_path=$(df --output=target | grep home)
 rsync -avz ${pvc_path}/jovyan /home
 
 while inotifywait -r -e modify,create,delete,move /home/jovyan; do
-    rsync -avz /home/jovyan $pvc_path
+    rsync -avz --delete /home/jovyan $pvc_path
 done
