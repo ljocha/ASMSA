ac747050198c7ba530ccbb7a76a36cc5747502c8
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-16 10:02:07 +0200

    added identificator

--- start.sh
+++ start.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-
+identificator=$JUPYTERHUB_SERVER_NAME
 pvc_path=$(df --output=target | grep home)
-rsync -avz ${pvc_path}/jovyan /home
+persist_folder=${pvc_path}/${identificator}
+rsync -avz $persist_folder /home
 
 function pvc_sync_daemon {
     while inotifywait -r -e modify,create,delete,move /home/jovyan; do
-        rsync -az --delete /home/jovyan $pvc_path
+        rsync -az --delete /home/jovyan $persist_folder
     done
 }
 
