ffdd9f5659b655291e5d5607a96d7b99b9da6349
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-17 10:08:25 +0200

    condition for init folder

--- start.sh
+++ start.sh
@@ -5,6 +5,11 @@ set -e
 identificator=$JUPYTERHUB_SERVER_NAME
 pvc_path=$(df --output=target | grep home)
 persist_folder="${pvc_path}/${identificator}"
+
+if [ ! -d "$persist_folder" ]; then
+  mkdir $persist_folder
+fi
+
 rsync -avz $persist_folder /home
 
 function pvc_sync_daemon {
