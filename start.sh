9637772ee2ae6549937a47e3209c722ca9d56f7a
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-17 10:22:08 +0200

    added trailing slashes & target

--- start.sh
+++ start.sh
@@ -4,17 +4,17 @@ set -e
 
 identificator=$JUPYTERHUB_SERVER_NAME
 pvc_path=$(df --output=target | grep home)
-persist_folder="${pvc_path}/${identificator}"
+persist_folder="${pvc_path}/${identificator}/"
 
 if [ ! -d "$persist_folder" ]; then
   mkdir $persist_folder
 fi
 
-rsync -avz $persist_folder /home
+rsync -avz $persist_folder /home/jovyan
 
 function pvc_sync_daemon {
     while inotifywait -r -e modify,create,delete,move /home/jovyan; do
-        rsync -az --delete /home/jovyan $persist_folder
+        rsync -az --delete /home/jovyan/ $persist_folder
     done
 }
 
