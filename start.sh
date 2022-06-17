7aaf95b524b281c1e9c34b8f4689507ab1adf7a2
Vladimír Višňovský <467814@mail.muni.cz>
2022-06-17 11:25:50 +0200

    explained code

--- start.sh
+++ start.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+# set persist directory based on HOSTNAME
+# which should always be unique
 identificator=$HOSTNAME
 pvc_path=$(df --output=target | grep home)
 persist_folder="${pvc_path}/${identificator}/"
 
+# create persist directory if it's the first run
 if [ ! -d "$persist_folder" ]; then
   mkdir $persist_folder
 fi
