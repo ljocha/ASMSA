00bc44b90bcf6d5928547e481a4d35fd6e9f472d
Vladimír Višňovský <467814@mail.muni.cz>
2022-08-15 11:17:23 +0200

    Deleted postBuild, tensorboardserverextension.py

--- tensorboardserverextension.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen
-
-
-def load_jupyter_server_extension(nbapp):
-    """serve the tensorboard with data from logs directory"""
-    Popen(["tensorboard", "--logdir", "logs", "--port", "6006"])
