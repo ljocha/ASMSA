fc364e12ef62107283d04a7d40e2238278a2c19e
Ales Krenek <ljocha@ics.muni.cz>
2024-02-23 19:47:04 +0100

    fixed working dir

--- gmx-wrap2.sh
+++ gmx-wrap2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# XXX: must be here
-img=$(cat IMAGE)
+# echo $0: "$@" >&2
+
+img=cerit.io/ljocha/gromacs:2023-2-plumed-2-9-afed-pytorch-model-cv
 
 # XXX: cwd root of the volue only
 
@@ -9,7 +10,10 @@ if [ -z "$DOCKER_WORKDIR" ]; then
 	pvcid=$(df . | tail -1 | cut -f1 -d ' ' | sed 's/^.*pvc-/pvc-/')
 	PVC=$(kubectl get pvc | grep $pvcid | cut -f1 -d' ')
 
-	kubectl get job/asmsa-gmx 2>/dev/null || kubectl apply -f - <<EOF
+	mnt=$(df . | tail -1 | cut -f7 -d ' ')
+	DIR=$(realpath --relative-base $mnt $PWD)
+
+	kubectl get job/asmsa-gmx 2>/dev/null >/dev/null || kubectl apply -f - >&2 <<EOF
 apiVersion: batch/v1
 kind: Job
 metadata:
@@ -60,8 +64,13 @@ spec:
           claimName: '$PVC'
 EOF
 
-	kubectl wait --for=condition=ready pod -l job=asmsa-gmx
-	kubectl exec -ti job/asmsa-gmx -- gmx "$@"
+    kcmd="cd /mnt/$DIR && gmx"
+    for c in "$@"; do
+        kcmd="$kcmd '$c'"
+    done
+
+	kubectl wait --for=condition=ready pod -l job=asmsa-gmx >&2
+	kubectl exec -ti job/asmsa-gmx -- bash -c "$kcmd"
 else
 	docker run -ti -v $DOCKER_WORKDIR:/work -w /work -u $(id -u) $img gmx "$@"
 fi
