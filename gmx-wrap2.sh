d03c3bb7067c096afd4c5f25f53994feb2dda32d
Ales Krenek <ljocha@ics.muni.cz>
2024-06-20 14:33:40 +0200

    volume specific sidecar job

--- gmx-wrap2.sh
+++ gmx-wrap2.sh
@@ -7,7 +7,8 @@ img=cerit.io/ljocha/gromacs:2023-2-plumed-2-9-afed-pytorch-model-cv-2
 
 input=$(cat -)
 
-# XXX: cwd root of the volue only
+# XXX: cwd root of the volume only
+gmxjob=asmsa-gmx-$$
 
 if [ -z "$DOCKER_WORKDIR" ]; then
 	pvcid=$(df . | tail -1 | cut -f1 -d ' ' | sed 's/^.*pvc-/pvc-/')
@@ -16,17 +17,23 @@ if [ -z "$DOCKER_WORKDIR" ]; then
 	mnt=$(df . | tail -1 | cut -f7 -d ' ')
 	DIR=$(realpath --relative-base $mnt $PWD)
 
-	kubectl get job/asmsa-gmx 2>/dev/null >/dev/null || kubectl apply -f - >&2 <<EOF
+	if [ -f $mnt/.gmxjob ]; then 
+		gmxjob=$(cat $mnt/.gmxjob)
+	else
+		echo $gmxjob >$mnt/.gmxjob
+	fi
+
+	kubectl get job/$gmxjob 2>/dev/null >/dev/null || kubectl apply -f - >&2 <<EOF
 apiVersion: batch/v1
 kind: Job
 metadata:
-  name: asmsa-gmx
+  name: $gmxjob
 spec:
   backoffLimit: 0
   template:
     metadata:
       labels:
-        job: asmsa-gmx
+        job: $gmxjob
     spec:
       restartPolicy: Never
       securityContext: # Pod security context
@@ -35,7 +42,7 @@ spec:
         seccompProfile:
           type: RuntimeDefault
       containers:
-      - name: asmsa-gmx
+      - name: $gmxjob
         image: $img
         workingDir: /mnt/
         command: [ 'sleep', 'inf' ] 
@@ -72,8 +79,8 @@ EOF
         kcmd="$kcmd '$c'"
     done
 
-	kubectl wait --for=condition=ready pod -l job=asmsa-gmx >&2
-	kubectl exec job/asmsa-gmx -- bash -c "$kcmd <<<\"$input\""
+	kubectl wait --for=condition=ready pod -l job=$gmxjob >&2
+	kubectl exec job/$gmxjob -- bash -c "$kcmd <<<\"$input\""
 else
 	docker run -i --gpus all -v $DOCKER_WORKDIR:/work -w /work -u $(id -u) $img gmx "$@" <<<"$input"
 fi
